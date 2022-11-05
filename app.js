f2ee0a113a587578a54cf80c8b3c99fcf49c9366
Abdur Rahim <99087922+rahimbd296345@users.noreply.github.com>
2022-11-05 08:16:32 +0600

    Add all files

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-function myFunction() {
-  getDocumentElement;
-}
+console.log('In The Name Of Allah, The Most Beneficent, The Most Merciful.');
+
+let x = 100
+x |= 5
+console.log('Value of x is: ' + x);
