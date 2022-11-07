834c6d619b0c347278b26c8bdec0c00a4f2f0f93
Abdur Rahim <99087922+rahimbd296345@users.noreply.github.com>
2022-11-07 09:07:21 +0600

    07.11.2022

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-console.log('In The Name Of Allah, The Most Beneficent, The Most Merciful.');
-// Write your code:
-
-
-
-myFunction()
-
-function myFunction() {
-    let carName = 'Volvo'
-    document.getElementById('demo').innerHTML = typeof carName + ' ' + carName;
+function displayDate() {
+  document.getElementById('demo').innerHTML = Date()
 }
-
-document.getElementById('demo1').innerHTML = typeof carName;
-
