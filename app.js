7afa2c1ed9d9204f007167e8644437affac08e05
Abdur Rahim <99087922+rahimbd296345@users.noreply.github.com>
2022-11-10 07:32:34 +0600

    Update now

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-function displayDate() {
-  document.getElementById('demo').innerHTML = Date()
+var whatDate = 'Sunday'
+
+if (whatDate === 'Friday'){
+  console.log('It is closed today');
+} else if (whatDate === 'Thursday'){
+  console.log('It is partially colsed');
+} else if (whatDate === 'Saturday'){
+  console.log('Some offices are closed');
+} else {
+  console.log('It is normal workong day!');
 }
