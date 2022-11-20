d0726b4eab07448d235567fb9a80ad0f41c83511
Abdur Rahim <99087922+rahimbd296345@users.noreply.github.com>
2022-11-20 10:39:04 +0600

    Learning Date

--- app.js
+++
@@ -1,6 +0,0 @@
-const fruits = ['Banana', 'Orange', 'Apple', 'Mango']
-document.getElementById('demo').innerHTML = fruits
-
-fruits.splice(2, 0, 'Lemon', 'kiwi')
-
-document.getElementById('demo').innerHTML = fruits
