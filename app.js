1267190a8861d0ecb03e595b428e3e143ab14c5f
Abdur Rahim <99087922+rahimbd296345@users.noreply.github.com>
2022-12-14 20:14:56 +0600

    12.12.2022

--- app.js
+++
@@ -1,9 +0,0 @@
-const fruits = new Map([
-  ['apples', 300],
-  ['bananas', 300],
-  ['oranges', 200]
-])
-
-fruits.set('apples', 200)
-
-document.getElementById('demo').innerHTML = fruits.get('apples')
